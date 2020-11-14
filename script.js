c288ededed71d55a1009282b31f7859e1967ddd7
RandomProgrammerGuy <parsa_farjam@yahoo.com>
2020-11-14 11:05:47 +0330

    Code rearrangement
    Rearranged JS Code to work better

--- script.js
+++ script.js
@@ -3,20 +3,16 @@ const board_background = "white";
 const snake_col = 'lightblue';
 const snake_border = 'darkblue';
 
+let snake = [{x: 200, y: 200}, {x: 200, y: 190}, {x: 200, y: 180}, {x: 200, y: 170}, {x: 200, y: 160}]
+
 const snakeboard = document.getElementById('gameBoard');
 const snakeboard_ctx = gameCanvas.getContext('2d');
 
-let snake = [{x: 200, y: 200}, {x: 200, y: 190}, {x: 200, y: 180}, {x: 200, y: 170}, {x: 200, y: 160}]
+main();
 
-function drawSnakePart(part) {
-    snakeboard_ctx.fillStyle = 'lightblue';
-    snakeboard_ctx.strokeStyle = 'darkblue';
-    snakeboard_ctx.fillRect(part.x, part.y, 10, 10);
-    snakeboard_ctx.strokeRect(part.x, part.y, 10, 10);
-}
-
-function drawSnake() {
-    snake.forEach(drawSnakePart)
+function main() {
+    clearCanvas();
+    drawSnake();
 }
 
 function clearCanvas() {
@@ -26,8 +22,16 @@ function clearCanvas() {
     snakeboard_ctx.strokeRect(0, 0, snakeboard.width, snakeboard.height);
 }
 
-function main() {
-    clearCanvas()
-    drawSnake()
+function drawSnakePart(part) {
+    snakeboard_ctx.fillStyle = 'lightblue';
+    snakeboard_ctx.strokeStyle = 'darkblue';
+    snakeboard_ctx.fillRect(part.x, part.y, 10, 10);
+    snakeboard_ctx.strokeRect(part.x, part.y, 10, 10);
 }
+
+function drawSnake() {
+    snake.forEach(drawSnakePart);
+}
+
+
 
