6561ad354f9508243c8b5d004495cf07b3d573ae
RandomProgrammerGuy <parsa_farjam@yahoo.com>
2020-11-14 10:48:46 +0330

    added drawSnake() function
    added two functions, one to draw a part of the snake and the other one to draw the whole snake.

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ const snakeboard = document.getElementById('gameBoard');
 const snakeboard_ctx = gameCanvas.getContext('2d');
 
 let snake = [{x: 200, y: 200}, {x: 200, y: 190}, {x: 200, y: 180}, {x: 200, y: 170}, {x: 200, y: 160}]
+
+function drawSnakePart(part) {
+    snakeboard_ctx.fillStyle = 'lightblue';
+    snakeboard_ctx.strokeStyle = 'darkblue';
+    snakeboard_ctx.fillRect(part.x, part.y, 10, 10);
+    snakeboard_ctx.strokeRect(part.x, part.y, 10, 10);
+}
+
+function drawSnake() {
+    snake.forEach(drawSnakePart)
+}
