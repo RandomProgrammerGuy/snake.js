f2b4c235ebe7a38b33e9ddd0bf2bb27d8d68ddac
RandomProgrammerGuy <parsa_farjam@yahoo.com>
2020-11-14 10:40:50 +0330

    Added Snake Element
    Added a let element including the beginning coordinates of the snake from {x: 200, y: 200} to {x:200, y: 160}

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 const snakeboard = document.getElementById('gameBoard');
 const snakeboard_ctx = gameCanvas.getContext('2d');
+
+let snake = [{x: 200, y: 200}, {x: 200, y: 190}, {x: 200, y: 180}, {x: 200, y: 170}, {x: 200, y: 160}]
